ed55294d75d09ebc1dd387f183e59f7fe7a20dde
PC Naylor <pcn500@sawtooth.york.ac.uk>
2015-07-31 13:06:12 +0100

    Merging changes

--- tools/pylib/visboutit/eigen.py
+++ tools/pylib/visboutit/eigen.py
@@ -15,21 +15,14 @@ import visual
 def plot_eigenvals(eigs, nx, ny, nz ,zshift, z_tol, ny2, name, pts ,data=None):
     fig = plt.figure() # Create a figure
     
-    if data is None:
-        # If no data supplied, only plot eigenvalues
-        ax = fig.add_subplot(111)
-    else:
-#         If data, plot two figures
-        ax = fig.add_subplot(111)
-        print "hi"
+    ax = fig.add_subplot(111) # Add a plot to the figure
     
     # Check that the data has the right size
     if len(data.shape) != 4:
         raise ValueError("Expecting data to be 2D")
     if data.shape[0] != len(eigs):
         raise ValueError("First dimension of data must match length of eigs")
-    
-    
+        
     eigs_r = eigs[:-1:2] # Collect the real eigenvalues
     eigs_i = eigs[1::2]    # Collect the imaginary eignevalues
     
@@ -41,18 +34,7 @@ def plot_eigenvals(eigs, nx, ny, nz ,zshift, z_tol, ny2, name, pts ,data=None):
     ax.set_ylabel("Imaginary component")
     
     overplot, = ax.plot([], [], 'ok') #Create an empty plot
-    
-    if data is not None: #If there is some data plot it
-        # Add a data plot
-        ax2 = fig.add_subplot(212)
-        vector_r, = ax2.plot([],[], '-k', label="Real")
-        vector_i, = ax2.plot([],[], '-r', label="Imag")
-        ax2.set_xlabel("X")
-        ax2.set_ylabel("Eigenvector")
-        ax2.legend()
-        
-    #Unpack variables
-        #  nx,ny,nz,zshift,z_tol,ny2,name,pts = data_variables
+
     def onclick(event):
         # Check if user clicked inside the plot
         if event.xdata is None:
@@ -74,13 +56,14 @@ def plot_eigenvals(eigs, nx, ny, nz ,zshift, z_tol, ny2, name, pts ,data=None):
             var_r = data[2*ind,:]
             var_i = data[2*ind+1,:]
             nx,ny,nz = visual.dimd(name)
-            #Interpolate the variables
+
+            # Interpolate the variables
             var_r2,ny3 = visual.zshift_interp3d(nx,ny,nz,zshift,z_tol,var_r)
             var_i2,ny3 = visual.zshift_interp3d(nx,ny,nz,zshift,z_tol,var_i)
-            #create the vtk variables
+            #c reate the vtk variables
             vrbl_r = visual.vtk_var(var_r2,nx,ny2,nz)
             vrbl_i = visual.vtk_var(var_i2,nx,ny2,nz)
-            #Write the vtk file
+            #W rite the vtk file
             
             vtk_path = visual.write_vtk_2(name,pts,vrbl_r,vrbl_i,ind)
             print "Eigenvalue %d written to vtk" % ind
@@ -92,26 +75,35 @@ def plot_eigenvals(eigs, nx, ny, nz ,zshift, z_tol, ny2, name, pts ,data=None):
 # Draw function calls the previous functions and collects the eigenvalue data,
 # writes x,y graph of the eigenvectors and then on clicking an eigen value the
 # eigenvalues are written to the vtk format.
-def draw(name,zshift_tol = 0.25):    
-    grid_file = str(raw_input('Enter Grid File : '))
-    path = os.chdir('/hwdisks/home/pcn500/Data/eigen/')
-    path = os.getcwd()
-    eigs = collect("t_array")
-    var = collect(name)
-    #import the variable, set the nx,ny,nz values
-    #write the pts variable
-    nx,ny,nz = visual.dimd(name)
+
+def draw(name,zshift_tol = 0.25, path = None):
+    # Set the work_dir
+    if path == None:
+        work_dir = os.getcwd()
+    if path != None:
+        work_dir = os.chdir(path)
+        work_dir = os.getcwd()
+
+    grid_file = visual.get("BOUT.inp","grid") # Import grid file
+
+    eigs = collect("t_array") # Import Eigen values
+    var = collect(name) # Import variable
+    nx,ny,nz = visual.dimd(name) # Set the shape of the variable
     #Import the R,Z values from the grid file
     r = visual.nc_var(grid_file,'Rxy') # R
     z = visual.nc_var(grid_file,'Zxy') # Z
-    #  print r.shape,z.shape
     zshift = visual.nc_var(grid_file, 'zShift') # zShift
-    z_tol = visual.z_shift_tol(nx,ny,zshift,zshift_tol)
-    #Interpolate r,z,zshift values
+    
+    z_tol = visual.z_shift_tol(nx,ny,zshift,zshift_tol) # set the z_tolerance value
+    
+    #Interpolate r, z, zshift values
+
     r2, ny2 = visual.zshift_interp2d(nx,ny,zshift,z_tol,r)
     z2, ny2 = visual.zshift_interp2d(nx,ny,zshift,z_tol,z)
     zshift2, ny2 = visual.zshift_interp2d(nx,ny,zshift,z_tol,zshift)
     
-    pts2 = visual.elm(r2,z2,zshift2,nx,ny2,nz)
-    #    Plot eigen values and write the associated vtk file
+    pts2 = visual.elm(r2,z2,zshift2,nx,ny2,nz) # Write the vtk points
+    
+    # Plot eigen values and write the associated vtk file
     plot_eigenvals(eigs, nx, ny, nz ,zshift, z_tol, ny2, name, pts2, var)
+
